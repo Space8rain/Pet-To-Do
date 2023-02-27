9d5aca5e0f46eda4031c714d8cbd3fd3aaab345c
NiNovikov_wpc <gambitrus38@yandex.ru>
2023-02-27 19:10:05 +0300

    add patch

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-let temp = document.querySelector('.template_task'),
+let patch = document.querySelector('.patch'),
+    temp = document.querySelector('.template_task'),
     container = document.querySelector('.tasks'),
     addButton = document.querySelector('.btn_add'),
     input = document.querySelector('.input');
 
-  const createNewTask = (taskText) => {
+  function createNewTask (taskText) {
   let cloneTemp = temp.content.querySelector('.task').cloneNode(true);
   cloneTemp.querySelector('.task_text').textContent = taskText;
   return cloneTemp
   };
 
+  function tasksCheker () {
+    patch.style.display = container.childElementCount < 1 ? 'block' : 'none'
+  }
+
   addButton.addEventListener('click', (evt) => {
     container.prepend(createNewTask(input.value));
     console.log(createNewTask(input.value));
+    input.value = ''
+    tasksCheker ();
+    console.log(container.childElementCount);
   })
 
-  console.log(temp);
+  // setTimeout(() => {
+  //   console.log(container.childElementCount);
+  // }, 1000);
+  tasksCheker ()
