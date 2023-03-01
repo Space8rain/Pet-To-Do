16ef0223f1911b3e4f168f140b0197ea2d9b9a11
NiNovikov_wpc <gambitrus38@yandex.ru>
2023-03-01 17:57:18 +0300

    refactoring

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
 const patch = document.querySelector('.patch'),
     temp = document.querySelector('.template_task'),
     container = document.querySelector('.tasks'),
-    addButton = document.querySelector('.btn_add'),
+    form = document.querySelector('.create_new_task'),
     input = document.querySelector('.input');
 
   function createNewTask (taskText) {
-  let cloneTemp = temp.content.querySelector('.task').cloneNode(true);
-  cloneTemp.querySelector('.task_text').textContent = taskText;
-  return cloneTemp
-  };
+  const taskHtml = `
+  <li class="task">
+    <div class="action">
+      <img src="./images/icon_drag.svg" alt="icon drag task"
+      class="btn_drag">
+      <input type="checkbox" class="task_done">
+    </div>
+    <p class="task_text">${taskText}</p>
+    <img src="./images/icon_task_del.svg" alt="icon delete task" class="task_del">
+  </li>`
 
-  function addTask() {
-    container.prepend(createNewTask(input.value));
-    input.value = '';
-    tasksCheker ();
+  return taskHtml
   };
-
-  function delTask(evt) {
-    const btn_del = evt.target.closest('.task_del');
-    if (!btn_del) return;
-    btn_del.parentElement.remove();
-    tasksCheker ();
-  }
 
   function tasksCheker () {
     patch.style.display = !container.childElementCount ? 'block' : 'none'
   };
 
-  addButton.addEventListener('click', addTask)
+  function addTask(event) {
+    event.preventDefault()
+    container.insertAdjacentHTML('beforeend', createNewTask(input.value));
+    input.value = '';
+    tasksCheker ();
+  };
+
+  function doneTask(event) {
+    const btnDone = event.target.closest('.task_done')
+    
+  }
+
+  function delTask(event) {
+    const btnDel = event.target.closest('.task_del');
+    if (!btnDel) return;
+    btnDel.parentElement.remove();
+    tasksCheker ();
+  };
+
+  form.addEventListener('submit', addTask)
 
   container.addEventListener('click', delTask)
